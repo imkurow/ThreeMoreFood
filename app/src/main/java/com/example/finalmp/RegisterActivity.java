c3025e6154bb884db50d3598502b03cd47d97504
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-15 01:16:53 +0700

    initialized FirebaseAuthentication

--- app/src/main/java/com/example/finalmp/RegisterActivity.java
+++ app/src/main/java/com/example/finalmp/RegisterActivity.java
@@ -13,6 +13,10 @@ import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class RegisterActivity extends AppCompatActivity {
 
     private EditText editTextFullname, editTextEmail, editTextPhone, editTextAddress, editTextPassword;
@@ -20,6 +24,8 @@ public class RegisterActivity extends AppCompatActivity {
     private Button buttonRegister;
     private TextView textViewLogin;
     private ProgressBar progressBar;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
 
 
@@ -27,6 +33,11 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        initViews();
+        setupClickListeners();g
     }
 
     private void initViews() {
