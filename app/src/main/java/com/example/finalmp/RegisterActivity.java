48bede1bc808d5448cb62320c32ed6a502fb9caa
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-14 08:15:58 +0700

    added InitViews method

--- app/src/main/java/com/example/finalmp/RegisterActivity.java
+++ app/src/main/java/com/example/finalmp/RegisterActivity.java
@@ -3,12 +3,39 @@ package com.example.finalmp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText editTextFullname, editTextEmail, editTextPhone, editTextAddress, editTextPassword;
+    private RadioGroup radioGroupGender;
+    private Button buttonRegister;
+    private TextView textViewLogin;
+    private ProgressBar progressBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
     }
+
+    private void initViews() {
+        editTextFullname = findViewById(R.id.editTextFullname);
+        editTextEmail = findViewById(R.id.editTextEmail);
+        editTextPhone = findViewById(R.id.editTextPhone);
+        editTextAddress = findViewById(R.id.editTextAddress);
+        editTextPassword = findViewById(R.id.editTextPassword);
+        radioGroupGender = findViewById(R.id.radioGroupGender);
+        buttonRegister = findViewById(R.id.buttonRegister);
+        textViewLogin = findViewById(R.id.textViewLogin);
+        progressBar = findViewById(R.id.progressBar);
+    }
+
 }
+
