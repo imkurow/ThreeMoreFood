0046786022640f5f1eea20e52e1fe7717d88dd16
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-16 11:48:19 +0700

    added Menu class Attributes

--- app/src/main/java/com/example/finalmp/Menu.java
+++ app/src/main/java/com/example/finalmp/Menu.java
@@ -1,5 +1,16 @@
 package com.example.finalmp;
 
 public class Menu {
+    private String id;
+    private String name;
+    private String description;
+    private String category;
+    private double price;
+    private String imageUrl;
+    private float rating;
+    private int orderCount;
+
+
+    public Menu(){}
     
 }
