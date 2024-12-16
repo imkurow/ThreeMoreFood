5034bb670c612095699de05ce6a46fb8626593cd
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-16 20:16:23 +0700

    added recyclerView dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,4 +52,5 @@ dependencies {
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation("com.github.bumptech.glide:glide:4.12.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.12.0")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
 }
