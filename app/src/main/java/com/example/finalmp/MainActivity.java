a2ebfa9eae48a6af541b3d75f39365d02fc8ae4d
Thierry Zackly <114277952+imkurow@users.noreply.github.com>
2024-12-16 11:43:14 +0700

    modified mainActivity, added some basics RecyclerView methods

--- app/src/main/java/com/example/finalmp/MainActivity.java
+++ app/src/main/java/com/example/finalmp/MainActivity.java
@@ -1,14 +1,135 @@
 package com.example.finalmp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+    private RecyclerView recyclerViewCategories;
+    private RecyclerView recyclerViewPopular;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Inisialisasi Firebase
+        mAuth = FirebaseAuth.getInstance();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // Setup UI
+        setupToolbar();
+        setupNavigationDrawer();
+        setupRecyclerViews();
+        loadUserData();
+        loadMenuData();
+    }
+
+    private void setupToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+    }
+
+    private void setupNavigationDrawer() {
+        drawerLayout = findViewById(R.id.drawerLayout);
+        navigationView = findViewById(R.id.navigationView);
+
+        navigationView.setNavigationItemSelectedListener(item -> {
+            // Handle navigation menu items
+            int id = item.getItemId();
+            if (id == R.id.nav_profile) {
+                startActivity(new Intent(MainActivity.this, ProfileActivity.class));
+            } else if (id == R.id.nav_cart) {
+                startActivity(new Intent(MainActivity.this, CartActivity.class));
+            } else if (id == R.id.nav_orders) {
+                // Handle orders
+            } else if (id == R.id.nav_logout) {
+                logout();
+            }
+            drawerLayout.closeDrawers();
+            return true;
+        });
+    }
+
+    private void setupRecyclerViews() {
+        // Setup Categories RecyclerView
+        recyclerViewCategories = findViewById(R.id.recyclerViewCategories);
+        recyclerViewCategories.setLayoutManager(
+                new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
+
+        // Setup Popular Menu RecyclerView
+        recyclerViewPopular = findViewById(R.id.recyclerViewPopular);
+        recyclerViewPopular.setLayoutManager(
+                new GridLayoutManager(this, 2));
+    }
+
+    private void loadUserData() {
+        FirebaseUser user = mAuth.getCurrentUser();
+        if (user != null) {
+            mDatabase.child("users").child(user.getUid())
+                    .addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            UserData userData = snapshot.getValue(UserData.class);
+                            if (userData != null) {
+                                updateNavigationHeader(userData);
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+                            Log.e("MainActivity", "Error loading user data", error.toException());
+                        }
+                    });
+        }
+    }
+
+    private void loadMenuData() {
+        // Load menu categories and popular items from Firebase
+        // Implement adapters and populate RecyclerViews
+    }
+
+    private void logout() {
+        mAuth.signOut();
+        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            drawerLayout.openDrawer(GravityCompat.START);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
