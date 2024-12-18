f3272e1d61f4f34b2e2ed8212c50c615d7e221f1
MakWil <marksutandi@gmail.com>
2024-12-18 22:45:58 +0700

    Created Order Status Class

--- app/src/main/java/com/example/finalmp/OrderStatus.java
+++ app/src/main/java/com/example/finalmp/OrderStatus.java
@@ -1,2 +1,19 @@
-package com.example.finalmp;public class OrderStatus {
+package com.example.finalmp;
+
+public enum OrderStatus {
+    PENDING("Menunggu Konfirmasi"),
+    PROCESSING("Sedang Diproses"),
+    ON_DELIVERY("Dalam Pengiriman"),
+    COMPLETED("Selesai"),
+    CANCELLED("Dibatalkan");
+
+    private final String displayName;
+
+    OrderStatus(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
 }
